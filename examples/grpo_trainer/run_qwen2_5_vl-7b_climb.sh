8fdc3b3d6669c21d3fcf925fe655b0082927efd9
DVD <zjdavid.2003@gmail.com>
2025-08-10 20:22:37 -0400

    disable monkey patch

--- examples/grpo_trainer/run_qwen2_5_vl-7b_climb.sh
+++ examples/grpo_trainer/run_qwen2_5_vl-7b_climb.sh
@@ -15,13 +15,14 @@ python3 -m verl.trainer.main_ppo \
     data.prompt_key=problem \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=1e-6 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
