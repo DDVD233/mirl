0abb4f96a0c62a9a7a24235397b6dd6d61ba20df
DVD <zjdavid.2003@gmail.com>
2025-08-15 07:52:48 -0400

    Increase worker count

--- examples/fairness/climb_vanilla_training.sh
+++ examples/fairness/climb_vanilla_training.sh
@@ -13,6 +13,7 @@ python3 -m verl.trainer.main_ppo \
     data.image_key=images \
     data.video_key=videos \
     data.prompt_key=problem \
+    data.dataloader_num_workers=16 \
     data.format_prompt=examples/format_prompt/default.jinja \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
