4b8098fb3408d8b69212d4514be37b3e951e990a
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-16 12:47:05 -0400

    _

--- examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -1,5 +1,8 @@
 set -x
 
+# Qwen/Qwen2.5-VL-7B-Instruct
+# Qwen/Qwen2.5-Omni-7B
+
 PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/train_no_meld_no_chalearn_vision_v2_template_prompts.jsonl \
@@ -16,7 +19,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 python3 -m verl.trainer.main_ppo \
     data.dataloader_num_workers=0 \
     data.modalities=\'audio,videos\' \
     data.format_prompt=/home/keaneong/human-behavior/verl/examples/format_prompt/default.jinja \
-    actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
