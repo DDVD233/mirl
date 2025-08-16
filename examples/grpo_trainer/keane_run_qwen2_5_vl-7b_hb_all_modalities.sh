d5d29cc01eeb14ff74b7c099fb3825a5bdcdbff0
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-16 18:54:17 -0400

    _

--- examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -41,7 +41,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.engine_kwargs.vllm.disable_mm_preprocessor_cache=True \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=True \
