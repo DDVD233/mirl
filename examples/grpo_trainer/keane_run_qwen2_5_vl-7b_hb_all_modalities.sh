1e46e85110e0a161d60180f1dc73e48d6eb78bc4
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-16 12:34:04 -0400

    _

--- examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -14,7 +14,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 python3 -m verl.trainer.main_ppo \
     data.video_key=videos \
     data.prompt_key=problem \
     data.dataloader_num_workers=0 \
-    data.modalities=\'videos\' \
+    data.modalities=\'audio,videos\' \
     data.format_prompt=/home/keaneong/human-behavior/verl/examples/format_prompt/default.jinja \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
