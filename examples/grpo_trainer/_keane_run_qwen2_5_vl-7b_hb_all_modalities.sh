4bd9a5c11577fec8116b58a84671dceff3a1df80
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-19 12:10:06 -0400

    _

--- examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -53,6 +53,12 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=3 \
+    actor_rollout_ref.model.lora_rank=32 \
+    actor_rollout_ref.model.lora_alpha=32 \
+    actor_rollout_ref.rollout.load_format=safetensors \
+    actor_rollout_ref.model.target_modules=all-linear \
+    actor_rollout_ref.model.use_shm=True \
+    actor_rollout_ref.rollout.layered_summon=True \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=1e-9 \
