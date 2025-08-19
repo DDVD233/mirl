f378c708d1b3edbfa1a26adefbfb0d9de8c33f58
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-19 11:27:24 -0400

    _

--- examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -37,8 +37,8 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     algorithm.adv_estimator=grpo \
     data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/0.1_train_no_lmvd_discretized_v3_template_prompts.jsonl  \
     data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/0.1_train_no_lmvd_discretized_v3_template_prompts.jsonl  \
-    data.train_batch_size=12 \
-    data.val_batch_size=12 \
+    data.train_batch_size=2 \
+    data.val_batch_size=2 \
     data.max_prompt_length=3072 \
     data.max_response_length=1536 \
     data.filter_overlong_prompts=False \
