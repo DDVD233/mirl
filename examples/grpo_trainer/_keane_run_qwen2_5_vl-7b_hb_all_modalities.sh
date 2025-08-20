eca7a4aced4a17adaa4e98004f11e0d0c6686ff6
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-19 22:42:23 -0400

    _

--- examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -48,7 +48,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     algorithm.adv_estimator=grpo \
     data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/train_video_only.jsonl \
     data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/val_vision_only.jsonl \
-    data.train_batch_size=513 \
+    data.train_batch_size=540 \
     data.val_batch_size=27 \
     data.max_prompt_length=4096 \
     data.max_response_length=4096 \
@@ -63,7 +63,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
-    actor_rollout_ref.actor.ppo_mini_batch_size=129 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=135 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=3 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0 \
