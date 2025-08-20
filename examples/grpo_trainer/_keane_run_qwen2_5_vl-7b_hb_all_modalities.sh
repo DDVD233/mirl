eaa2f402baac89e916c9658c71b3c0a3ad59c4f9
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-19 21:20:01 -0400

    _

--- examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_keane_run_qwen2_5_vl-7b_hb_all_modalities.sh
@@ -46,7 +46,7 @@ unset ROCR_VISIBLE_DEVICES
 
 PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/verl:$PYTHONPATH" NCCL_ASYNC_ERROR_HANDLING=1 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
-    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/train_video_only.jsonl \
+    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/train_image_only.jsonl \
     data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/val_vision_only.jsonl \
     data.train_batch_size=512 \
     data.val_batch_size=27 \
