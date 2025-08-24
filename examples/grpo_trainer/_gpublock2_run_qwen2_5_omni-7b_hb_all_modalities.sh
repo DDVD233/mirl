a8a5a7bc8d914831d1580758fe32edfe35a90133
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-24 09:26:33 -0400

    _

--- examples/grpo_trainer/_gpublock2_run_qwen2_5_omni-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_gpublock2_run_qwen2_5_omni-7b_hb_all_modalities.sh
@@ -46,7 +46,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.engine_kwargs.vllm.disable_mm_preprocessor_cache=True \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=True \
