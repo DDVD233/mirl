ba02a22845d0eb6aaded99ec960dfa115cf45b01
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-24 20:51:17 -0400

    _

--- examples/grpo_trainer/_train_one_dataset_run_qwen2_5_omni-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_train_one_dataset_run_qwen2_5_omni-7b_hb_all_modalities.sh
@@ -81,8 +81,8 @@ export CUDA_VISIBLE_DEVICES=0,1
 
 PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/verl:$PYTHONPATH" NCCL_ASYNC_ERROR_HANDLING=1 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
-    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/train_chsimsv2_only.jsonl \
-    data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/val_chsimsv2_only.jsonl \
+    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_train_chsimsv2_only.jsonl \
+    data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_val_chsimsv2_only.jsonl \
     data.train_batch_size=240 \
     data.val_batch_size=120 \
     data.max_prompt_length=4096 \
@@ -100,7 +100,7 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     data.val_modality_batching.drop_last=False \
     data.format_prompt=/home/keaneong/human-behavior/verl/examples/format_prompt/default.jinja \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
-    actor_rollout_ref.actor.optim.lr=1e-8 \
+    actor_rollout_ref.actor.optim.lr=1e-7 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=60 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
@@ -131,15 +131,15 @@ PYTHONUNBUFFERED=1 HYDRA_FULL_ERROR=1 PYTHONPATH="/home/keaneong/human-behavior/
     reward_model.reward_manager=batch \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='full_chsimsv2_mixed_modal_omni' \
-    trainer.experiment_name='3_full_chsimsv2_mixed_modal_omni' \
+    trainer.project_name='add_audio_chsimsv2_hb_omni' \
+    trainer.experiment_name='add_audio_chsimsv2_hb_omni ' \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.val_before_train=False \
     trainer.val_only=False \
-    trainer.validation_data_dir=/home/keaneong/human-behavior/verl/examples/grpo_trainer/3_val_full_chsimsv2_hb_omni \
+    trainer.validation_data_dir=/home/keaneong/human-behavior/verl/examples/grpo_trainer/val_add_audio_chsimsv2_hb_omni \
     trainer.test_freq=2 \
     trainer.total_epochs=10 $@ \
-    trainer.default_local_dir=/scratch/keane/human_behaviour/3_full_chsimsv2_mixed_modal_omni \
+    trainer.default_local_dir=/scratch/keane/human_behaviour/add_audio_chsimsv2_hb_omni \
     # trainer.resume_mode=auto \
