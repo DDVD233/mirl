201428749afca428787c23c20b5fed3d5be41695
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-28 19:31:45 -0400

    _

--- multi_task_classification/train_head_only.sh
+++ multi_task_classification/train_head_only.sh
@@ -22,7 +22,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --val_batch_size 2 \
     --lr 1e-4 \
     --epochs 2 \
-    --save_every_n_epochs 9 \
+    --save_every_n_epochs 1 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/2_head_only_training" \
     --load_checkpoint_path "/scratch/keane/human_behaviour/2_head_only_training/checkpoint_epoch_1.pt" \
     --validate_every_n_epochs 1 \
