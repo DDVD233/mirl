b9afe1e950d47e1a16e2893fb34ea7c638b86792
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-28 19:15:18 -0400

    _

--- multi_task_classification/train_head_only.sh
+++ multi_task_classification/train_head_only.sh
@@ -13,8 +13,7 @@ echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 
-    # --load_checkpoint_path "/scratch/keane/human_behaviour/2_head_only_training/checkpoint_epoch_1.pt" \
-
+   
 # Launch training with accelerate for head_only strategy
 echo "Launching head_only training with Accelerate..."
 accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
@@ -23,8 +22,9 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --val_batch_size 2 \
     --lr 1e-4 \
     --epochs 2 \
-    --save_every_n_epochs 1 \
+    --save_every_n_epochs 9 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/2_head_only_training" \
+    --load_checkpoint_path "/scratch/keane/human_behaviour/2_head_only_training/checkpoint_epoch_1.pt" \
     --validate_every_n_epochs 1 \
     --validate_every_n_steps None \
     --early_stopping_patience 99999 \
