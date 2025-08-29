61be109d2c237cc3a331b4ad8186c17156ee69d4
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-29 13:47:27 -0400

    _

--- multi_task_classification/debug_train_head_only.sh
+++ multi_task_classification/debug_train_head_only.sh
@@ -29,7 +29,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --val_file "/scratch/keane/human_behaviour/human_behaviour_data/0.01_audio_sigs_train_meld.jsonl" \
     --test_file "/scratch/keane/human_behaviour/human_behaviour_data/0.01_audio_sigs_train_meld.jsonl" \
     --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/meld_label_map.json" \
-    --save_every_n_epochs 99 \
+    --save_every_n_epochs 1 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/3_debug_head_only_training" \
     --validate_every_n_epochs None \
     --validate_every_n_steps 100 \
