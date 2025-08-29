438f1a34997a6d6e309f715853974ba925a71afb
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-29 18:35:17 -0400

    _

--- multi_task_classification/train_lora_all.sh
+++ multi_task_classification/train_lora_all.sh
@@ -33,7 +33,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --validate_every_n_steps 1000 \
     --validate_every_n_epochs 1 \
     --early_stopping_patience 99999999 \
-    --project "omni-classifier-lora" \
+    --project "omni-classifier-lora-full" \
     --gradient_accumulation_steps 16 \
 
 echo "LoRA training completed!"
