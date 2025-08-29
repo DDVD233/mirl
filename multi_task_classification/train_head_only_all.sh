12c4d489ab72bd84ba6599cd8190bb9b82f6ee3b
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-29 16:10:56 -0400

    _

--- multi_task_classification/train_head_only_all.sh
+++ multi_task_classification/train_head_only_all.sh
@@ -21,10 +21,10 @@ export TORCH_USE_CUDA_DSA=1
 echo "Launching head_only training with Accelerate..."
 accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --training_strategy head_only \
-    --train_file "/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_fixed_full_train.jsonl" \
-    --val_file "/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_fixed_full_val.jsonl" \
-    --test_file "/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_fixed_full_test.jsonl " \
-    --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/full_label_map.json" \
+    --train_file "/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_no_chalearn_no_expw_no_mosei_fixed_0.1_train.jsonl" \
+    --val_file "/scratch/keane/human_behaviour/human_behaviour_data/aaudio_sigs_no_chalearn_no_expw_no_mosei_fixed_0.1_val.jsonl" \
+    --test_file "/scratch/keane/human_behaviour/human_behaviour_data/audio_sigs_no_chalearn_no_expw_no_mosei_fixed_0.1_test.jsonl " \
+    --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/0.1_no_chalearn_no_expw_no_mosei_label_map.json" \
     --train_batch_size 2 \
     --val_batch_size 2 \
     --lr 5e-3 \
