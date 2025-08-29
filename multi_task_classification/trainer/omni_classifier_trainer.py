41221a5f23460e5d115842f679c382b516e0a69d
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-29 13:33:32 -0400

    debug

--- multi_task_classification/trainer/omni_classifier_trainer.py
+++ multi_task_classification/trainer/omni_classifier_trainer.py
@@ -220,6 +220,11 @@ class OmniClassifierAccelerateTrainer:
         
         with open("/home/keaneong/human-behavior/verl/multi_task_classification/classifier_states.txt", "a") as f:
                 f.write(f"\nNEW Accelerator saved state")
+                f.write(f"\n{ckpt_dir}")
+                f.write(f"\n{global_step}")
+                f.write(f"\n{len_dl}")
+                f.write(f"\n{start_epoch}")
+                f.write(f"\n{start_batch_offset}")
                 raise Exception("Stop here")
         
         return start_epoch, start_batch_offset, global_step, meta, ckpt_dir
