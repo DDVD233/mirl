0f2654b6224c1d1e883701548b1e84275cc18982
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-08-31 14:53:52 -0400

    _

--- multi_task_classification/train_lora_all_unified.sh
+++ multi_task_classification/train_lora_all_unified.sh
@@ -14,10 +14,6 @@ export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 
 
-#   --use_scheduler \
-#     --scheduler_type cosine \
-#     --warmup_steps 50
-
 # NOTE A WARMUP STEP HERE IS NOT A BATCH, IT IS A GRADIENT ACCUMULATION STEP
 
 # Launch training with accelerate for LoRA strategy
@@ -32,12 +28,15 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train.py \
     --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/final_unified_scheme_binarymmpsy_no_vptd_chalearn_lmvd_esconv_full_label_map.json" \
     --lr 1e-4 \
     --epochs 10 \
-    --save_checkpoint_dir "/scratch/keane/human_behaviour/2_unified_scheme_full_lora_training" \
+    --save_checkpoint_dir "/scratch/keane/human_behaviour/2_lr_unified_scheme_full_lora_training" \
     --validate_every_n_steps None \
     --validate_every_n_epochs 1 \
     --save_every_n_steps 10000 \
     --early_stopping_patience 99999999 \
     --project "omni-classifier-lora-full-unified" \
     --gradient_accumulation_steps 128 \
+    --use_scheduler \
+    --scheduler_type cosine \
+    --warmup_steps 50
 
 echo "LoRA training completed!"
