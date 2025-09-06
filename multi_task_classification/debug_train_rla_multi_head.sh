f66e75986f2efaa08eca110c8fcedfa498e365ac
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-05 22:40:11 -0400

    _

--- multi_task_classification/debug_train_rla_multi_head.sh
+++ multi_task_classification/debug_train_rla_multi_head.sh
@@ -38,6 +38,7 @@ export TORCH_USE_CUDA_DSA=1
   # --rla_video_alpha_init 2.0 \
   # --rla_audio_alpha_init 2.0 \
 
+  # --load_checkpoint_path "/scratch/keane/human_behaviour/debug_rla/step_1998" \
 
 accelerate launch --config_file configs/accelerate_config_qwen.yaml train_rla_multi_head.py \
   --mode train \
@@ -48,38 +49,37 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train_rla_mu
   --test_batch_size 2 \
   --lr 1e-4 \
   --hard_gamma 0.0 \
-  --base_lr 1e-5 \
+  --base_lr 1e-4 \
   --rla_lr  5e-4 \
   --epochs 10 \
-  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/0.2_feat_meld_train.jsonl" \
+  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_train.jsonl" \
   --val_file   "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_val.jsonl" \
   --test_file  "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_test.jsonl" \
   --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_feat_meld_label_map.json" \
   --save_every_n_epochs 1 \
   --save_every_n_steps 99999999 \
-  --load_checkpoint_path "/scratch/keane/human_behaviour/debug_rla/step_1998" \
-  --save_checkpoint_dir "/scratch/keane/human_behaviour/resume_debug_rla_residual_res_only" \
-  --validation_result_dir "/scratch/keane/human_behaviour/debug_rla_residual_res_only/validation_results" \
+  --save_checkpoint_dir "/scratch/keane/human_behaviour/base_only_rla" \
+  --validation_result_dir "/scratch/keane/human_behaviour/base_only_rla/validation_results" \
   --validate_every_n_epochs 1 \
   --validate_every_n_steps 999999 \
   --early_stopping_patience 99999 \
   --project "debug-rla-omni-classifier-multi-head-lora" \
   --gradient_accumulation_steps 8 \
-  --rla_stage joint \
+  --rla_stage base_only \
   --use_rla_video \
   --use_rla_audio \
   --d_video_feat 3318 \
   --d_audio_feat 6373 \
   --rla_hidden_video 256 \
-  --rla_hidden_audio 512 \
-  --rla_p_moddrop_video 0.20 \
-  --rla_p_moddrop_audio 0.20 \
+  --rla_hidden_audio 256 \
+  --rla_p_moddrop_video 0 \
+  --rla_p_moddrop_audio 0 \
   --rla_video_temporal meanstd \
   --rla_video_norm none \
   --rla_audio_norm l2 \
   --rla_audio_temporal none \
-  --rla_video_alpha_init 2.0 \
-  --rla_audio_alpha_init 2.0 \
+  --rla_video_alpha_init 3.0 \
+  --rla_audio_alpha_init 3.0 \
   --rla_video_use_ln \
   --rla_audio_use_ln    
 
