ce5137f381d100b48a8ecbf82bc50369b13a17cb
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-06 23:09:39 -0400

    _

--- multi_task_classification/debug_train_rla_multi_head.sh
+++ multi_task_classification/debug_train_rla_multi_head.sh
@@ -53,7 +53,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train_rla_mu
   --base_lr 1e-4 \
   --rla_lr  5e-5 \
   --epochs 10 \
-  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/0.2_feat_meld_train.jsonl" \
+  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_train.jsonl" \
   --val_file   "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_val.jsonl" \
   --test_file  "/scratch/keane/human_behaviour/human_behaviour_data/feat_meld_test.jsonl" \
   --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_feat_meld_label_map.json" \
