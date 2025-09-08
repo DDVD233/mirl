a399d88ab8fe3a857de4621db058e986e3d1daa5
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-07 20:44:06 -0400

    fix

--- multi_task_classification/train_lora_multi_head.sh
+++ multi_task_classification/train_lora_multi_head.sh
@@ -43,6 +43,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train_multi_
     --save_every_n_steps 5000 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/v5_multi_head_lora_training" \
     --validation_result_dir "/scratch/keane/human_behaviour/v5_multi_head_lora_training/validation_results" \
+    --load_checkpoint_path "/scratch/keane/human_behaviour/v5_multi_head_lora_training/step_20000" \
     --validate_every_n_epochs 1 \
     --validate_every_n_steps 9999999 \
     --early_stopping_patience 99999999 \
