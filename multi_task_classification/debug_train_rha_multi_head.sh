eb1970f6a874e00c12a7660568b1085ffc8c9f45
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-10 12:33:21 -0400

    debug

--- multi_task_classification/debug_train_rha_multi_head.sh
+++ multi_task_classification/debug_train_rha_multi_head.sh
@@ -44,6 +44,7 @@ export TORCH_USE_CUDA_DSA=1
 # --rla_resume_diff_training_stage \
 
 accelerate launch --config_file configs/accelerate_config_qwen.yaml train_rha_multi_head.py \
+  --rla_resume_diff_training_stage \
   --mode train \
   --training_strategy lora \
   --train_batch_size 2 \
