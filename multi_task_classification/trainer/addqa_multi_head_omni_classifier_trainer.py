d5c1b882c6d415e7e2e32571fd206583bc0110cc
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-10 09:28:38 -0400

    fix

--- multi_task_classification/trainer/addqa_multi_head_omni_classifier_trainer.py
+++ multi_task_classification/trainer/addqa_multi_head_omni_classifier_trainer.py
@@ -935,7 +935,7 @@ class MultiHeadOmniClassifierAccelerateTrainer:
 
                     # Step-based validation (if configured)
                     if validate_every_n_steps is not None and current_step % validate_every_n_steps == 0:
-                        self.accelerator.wait_for_everyone()
+
                         print(f"\n[STEP {current_step}] Running step-based validation...")
                         val_results = self.validate(val_dataloader, "validation", current_step=current_step)
                         # val_results = self.validate_off_accelerate_with_generate(val_dataloader_raw, "validation", current_step)
