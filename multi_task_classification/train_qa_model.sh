d3d436a69cc3a69109359441171fb0bfa9462431
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-10 09:09:24 -0400

    fix

--- multi_task_classification/train_qa_model.sh
+++ multi_task_classification/train_qa_model.sh
@@ -29,13 +29,13 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml addqa_train_
     --val_file  "/scratch/keane/human_behaviour/human_behaviour_data/qa_test.jsonl" \
     --test_file "/scratch/keane/human_behaviour/human_behaviour_data/qa_test.jsonl" \
     --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_label_map_w_feats_v5_unified_scheme_splitmmpsy_binarymmpsy_no_vptd_chalearn_lmvd_esconv.json" \
-    --load_checkpoint_path "/scratch/keane/human_behaviour/v5_multi_head_lora_training/step_49500" \
+    --load_checkpoint_path "/scratch/keane/human_behaviour/2_qa_multi_task_model/step_4500" \
     --save_every_n_epochs 1 \
-    --save_every_n_steps 4500 \
+    --save_every_n_steps 9999999 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/2_qa_multi_task_model" \
     --validation_result_dir "/scratch/keane/human_behaviour/2_qa_multi_task_model/test_results" \
     --validate_every_n_epochs 1 \
-    --validate_every_n_steps 99999 \
+    --validate_every_n_steps 1 \
     --early_stopping_patience 999999 \
     --project "qa-omni-classifier-multi-task-lora" \
     --gradient_accumulation_steps 8 \
