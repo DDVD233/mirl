13885f7e588a2579c177a2a0448cd45c3c9f0fd9
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-10 12:40:04 -0400

    debug

--- multi_task_classification/trainer/rha_multi_head_omni_classifier_trainer.py
+++ multi_task_classification/trainer/rha_multi_head_omni_classifier_trainer.py
@@ -954,6 +954,8 @@ class RHAMultiHeadOmniClassifierAccelerateTrainer:
                 
                 with self.accelerator.accumulate(self.model):
                     # 1) pooled hidden once
+                    logits = self.model(input_ids, attention_mask=attention_mask, domain_ids=domain_ids)
+                    raise Exception("check here")
                     pooled = self.model.encode(input_ids, attention_mask=attention_mask)  # [B,H]
 
                     # 2) prelim logits (for RHA confidence)
