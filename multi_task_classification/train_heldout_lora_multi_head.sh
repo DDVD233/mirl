fea2418f11bf9434adcc4fd7d1021ea07dfa11db
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-14 15:34:07 -0400

    _

--- multi_task_classification/train_heldout_lora_multi_head.sh
+++ multi_task_classification/train_heldout_lora_multi_head.sh
@@ -40,7 +40,7 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train_multi_
     --save_every_n_epochs 1 \
     --save_every_n_steps 5000 \
     --save_checkpoint_dir "/scratch/keane/human_behaviour/v6_heldout_multi_head_lora_training" \
-    --load_checkpoint_path "/scratch/keane/human_behaviour/v6_heldout_multi_head_lora_training/step_10000" \
+    --load_checkpoint_path "/scratch/keane/human_behaviour/v6_heldout_multi_head_lora_training/step_45000" \
     --validation_result_dir "/scratch/keane/human_behaviour/v6_heldout_multi_head_lora_training/validation_results" \
     --validate_every_n_epochs 1 \
     --validate_every_n_steps 9999999 \
