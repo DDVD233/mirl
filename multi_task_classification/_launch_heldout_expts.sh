50706ed2b9c5ab72272ecb0f46a043ee453cd7c8
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-15 10:53:15 -0400

    _

--- multi_task_classification/_launch_heldout_expts.sh
+++ multi_task_classification/_launch_heldout_expts.sh
@@ -6,7 +6,7 @@ set -u
 echo "Starting LoRA sweep (CLS + QA)…"
 
 # GPUs / perf
-export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="4,5"
 echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
