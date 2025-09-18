44d838f6f214d607d22a9a842c737205fa14ef9d
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-17 20:33:35 -0400

    _

--- multi_task_classification/train_qa_model.sh
+++ multi_task_classification/train_qa_model.sh
@@ -27,22 +27,25 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml addqa_train_
     --val_batch_size 2 \
     --test_batch_size 2 \
     --lr 1e-4 \
-    --epochs 5 \
+    --epochs 3 \
     --train_file "/scratch/keane/human_behaviour/human_behaviour_data/qa_train.jsonl" \
     --val_file  "/scratch/keane/human_behaviour/human_behaviour_data/qa_test.jsonl" \
     --test_file "/scratch/keane/human_behaviour/human_behaviour_data/qa_test.jsonl" \
-    --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_label_map_w_feats_v5_unified_scheme_splitmmpsy_binarymmpsy_no_vptd_chalearn_lmvd_esconv.json" \
-    --load_checkpoint_path "/scratch/keane/human_behaviour/3_freeze_base_qa_multi_task_model/step_12000" \
+    --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_label_map_v6.json" \
+    --load_checkpoint_path "/scratch/keane/human_behaviour/v6_multi_head_lora_training/step_43539" \
     --save_every_n_epochs 1 \
     --save_every_n_steps 2000 \
-    --save_checkpoint_dir "/scratch/keane/human_behaviour/3_freeze_base_qa_multi_task_model/" \
-    --validation_result_dir "/scratch/keane/human_behaviour/3_freeze_base_qa_multi_task_model/test_results" \
+    --save_checkpoint_dir "/scratch/keane/human_behaviour/4_freeze_base_qa_multi_task_model/" \
+    --validation_result_dir "/scratch/keane/human_behaviour/4_freeze_base_qa_multi_task_model/test_results" \
     --validate_every_n_epochs 1 \
     --validate_every_n_steps 999999 \
     --early_stopping_patience 999999 \
-    --project "2_freeze-base-qa-omni-classifier-multi-task-lora" \
+    --project "4_freeze-base-qa-omni-classifier-multi-task-lora" \
     --gradient_accumulation_steps 8 \
     --format_prompt "" \
     --max_prompt_length 8096 \
+    --use_scheduler \
+    --scheduler_type cosine \
+    --warmup_steps 50 \
 
 echo "QA training completed!"
