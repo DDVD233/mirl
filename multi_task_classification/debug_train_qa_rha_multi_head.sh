36e7537dd07e9f8d5db1158d3c0a11c78d26a658
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-09-21 17:06:05 -0400

    _

--- multi_task_classification/debug_train_qa_rha_multi_head.sh
+++ multi_task_classification/debug_train_qa_rha_multi_head.sh
@@ -72,14 +72,14 @@ accelerate launch --config_file configs/accelerate_config_qwen.yaml train_qa_rha
   --base_lr 1e-4 \
   --rla_lr  5e-4 \
   --epochs 1 \
-  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/last_train_siq2_rha.jsonl" \
-  --val_file   "/scratch/keane/human_behaviour/human_behaviour_data/last_test_siq2_rha.jsonl" \
-  --test_file  "/scratch/keane/human_behaviour/human_behaviour_data/last_test_siq2_rha.jsonl" \
+  --train_file "/scratch/keane/human_behaviour/human_behaviour_data/last_train_intentqa_rha.jsonl" \
+  --val_file   "/scratch/keane/human_behaviour/human_behaviour_data/last_test_intentqa_rha.jsonl" \
+  --test_file  "/scratch/keane/human_behaviour/human_behaviour_data/last_test_intentqa_rha.jsonl" \
   --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/unified_label_map_v6.json" \
   --save_every_n_epochs 1 \
   --save_every_n_steps 2000 \
   --save_checkpoint_dir "/scratch/keane/human_behaviour/last_rha_freeze_base_qa_multi_task_model" \
-  --validation_result_dir "/scratch/keane/human_behaviour/last_rha_freeze_base_qa_multi_task_model/siq2_test_results" \
+  --validation_result_dir "/scratch/keane/human_behaviour/last_rha_freeze_base_qa_multi_task_model/intentqs_test_results" \
   --load_checkpoint_path "/scratch/keane/human_behaviour/4_freeze_base_qa_multi_task_model/step_4578" \
   --validate_every_n_epochs 1 \
   --validate_every_n_steps 999999 \
