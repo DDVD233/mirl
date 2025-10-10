cd0b03d4d6c90254f901b9f90fe2a99563676ed8
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-10-10 13:56:52 -0400

    update hb script

--- examples/grpo_trainer/_gpublock1_run_qwen2_5_omni-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_gpublock1_run_qwen2_5_omni-7b_hb_all_modalities.sh
@@ -11,8 +11,8 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
-    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/fixed_0.1_train.jsonl \
-    data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/fixed_0.1_val.jsonl \
+    data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/w_feats_v6_train.jsonl \
+    data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/w_feats_v6_val.jsonl \
     data.train_batch_size=192 \
     data.val_batch_size=96 \
     data.max_prompt_length=4096 \
@@ -30,7 +30,7 @@ python3 -m verl.trainer.main_ppo \
     data.val_modality_batching.drop_last=False \
     data.format_prompt=/home/keaneong/human-behavior/verl/examples/format_prompt/default.jinja \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
-    actor_rollout_ref.actor.optim.lr=1e-7 \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=48 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
