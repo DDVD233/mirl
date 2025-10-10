e3d8233a5d185cc84f5a955d7b0a1bc9a9a29cd8
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-10-10 14:07:38 -0400

    _

--- examples/grpo_trainer/_gpublock1_run_qwen2_5_omni-7b_hb_all_modalities.sh
+++ examples/grpo_trainer/_gpublock1_run_qwen2_5_omni-7b_hb_all_modalities.sh
@@ -53,8 +53,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-    actor_rollout_ref.rollout.max_model_len=4096 \
-    actor_rollout_ref.rollout.max_num_batched_tokens=4096 \
+    actor_rollout_ref.rollout.max_model_len=8096 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=8096 \
     algorithm.use_kl_in_reward=False \
     custom_reward_function.path=/home/keaneong/human-behavior/verl/examples/reward_function/human_behaviour.py \
     custom_reward_function.name=human_behaviour_compute_score_batch \
