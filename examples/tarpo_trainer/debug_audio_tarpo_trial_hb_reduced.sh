64829f96686438df9926d7009b29c065013c056b
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-10-29 11:14:24 -0400

    _

--- examples/tarpo_trainer/debug_audio_tarpo_trial_hb_reduced.sh
+++ examples/tarpo_trainer/debug_audio_tarpo_trial_hb_reduced.sh
@@ -20,7 +20,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 
     python3 -m verl.trainer.main_ppo \
         algorithm.adv_estimator=grpo \
-        data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/qa_train_w_feats.jsonl \
+        data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/problematic_lines.jsonl \
         data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/qa_test_w_feats.jsonl \
         data.train_batch_size=128 \
         data.val_batch_size=64 \
