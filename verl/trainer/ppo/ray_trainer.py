cf7c166f327ff366c3501679038b5a822712204a
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-11-04 07:05:47 +0800

    _

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1237,6 +1237,7 @@ class RayPPOTrainer:
 
         # load dataloader,
         # TODO: from remote not implemented yet
+        # TODO_TAPRO: can you check if the dataloader is loaded correctly for our case
         dataloader_local_path = os.path.join(global_step_folder, "data.pt")
         if os.path.exists(dataloader_local_path):
             dataloader_state_dict = torch.load(dataloader_local_path, weights_only=False)
