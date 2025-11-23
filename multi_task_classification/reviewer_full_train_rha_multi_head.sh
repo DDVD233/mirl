b6790ac5b8469e53e00c95122e8f8ca4a0cfec22
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-11-23 16:35:14 -0500

    _

--- multi_task_classification/reviewer_full_train_rha_multi_head.sh
+++ multi_task_classification/reviewer_full_train_rha_multi_head.sh
@@ -212,7 +212,7 @@ for DS in "${PROCESS_DS[@]}"; do
     --base_lr 1e-4 \
     --rla_lr 5e-4 \
     --epochs 3 \
-    --train_file "$TRAIN_OUT" \
+    --train_file /scratch/keane/human_behaviour/human_behaviour_data/trunc_rla_fulltemp_train_daicwoz.jsonl \
     --val_file "$VAL_OUT" \
     --test_file "$VAL_OUT" \
     --label_map_path "/home/keaneong/human-behavior/verl/multi_task_classification/label_maps/unified_label_map_v6.json" \
