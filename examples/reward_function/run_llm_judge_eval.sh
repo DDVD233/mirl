7dc095ae71b1d99d9da4ae21e5a545d16c72fff0
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-12-03 09:06:17 -0800

    change parameter llm

--- examples/reward_function/run_llm_judge_eval.sh
+++ examples/reward_function/run_llm_judge_eval.sh
@@ -6,8 +6,11 @@
 
 # Default values
 # PLEASE PUT THE RESULTS_PATH as the path within the save dir that points to the generated outputs for all questions, which should look like "full_test_or_val_generation_outputs/150.json"
-RESULTS_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/input_data_2025-11-28_11-53-44.json"
-SAVE_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/results_temp.json"
+# RESULTS_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/input_data_2025-11-28_11-53-44.json"
+# SAVE_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/results_temp.json"
+
+RESULTS_PATH="/scratch/keane/human_behaviour/tarpo_iter_6_mixtures/full_test_or_val_generation_outputs/step_200.json"
+SAVE_PATH="/scratch/keane/human_behaviour/tarpo_iter_6_mixtures/full_test_or_val_generation_outputs/step_200_results.json"
 PROVIDER="openai"
 WANDB_PROJECT="llm_judge_eval"
 WANDB_RUN_NAME="experiment_$(date +%Y%m%d_%H%M%S)"
