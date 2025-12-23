885c7b35605a1f9b67db68d10bd6576713bc98b2
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-12-23 21:47:26 +0800

    _

--- examples/tarpo_trainer/_qwen3_tarpo_full_trial_4gpus.sh
+++ examples/tarpo_trainer/_qwen3_tarpo_full_trial_4gpus.sh
@@ -37,7 +37,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 # /scratch/keane/human_behaviour/human_behaviour_data/final_v8_train_cleaned_2.jsonl
     python3 -m verl.trainer.main_ppo \
         algorithm.adv_estimator=tarpo \
-        data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/test_mimeqa.jsonl \
+        data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/qa_train_w_feats.jsonl\
         data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_val_cleaned.jsonl \
         data.train_batch_size=256 \
         data.val_batch_size=64 \
