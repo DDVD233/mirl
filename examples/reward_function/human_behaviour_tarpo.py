db5355c20a72ee13ec1fafe389618a7b2b6ec541
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-12-24 11:51:46 +0800

    _

--- examples/reward_function/human_behaviour_tarpo.py
+++ examples/reward_function/human_behaviour_tarpo.py
@@ -185,6 +185,7 @@ def human_behaviour_compute_score_batch(
             "overlong_score": overlong_score,   # 0..-1 (0 if disabled)
             "task_type": task_type,
         })
+    return overall_score
     return batch_scores
 
 
