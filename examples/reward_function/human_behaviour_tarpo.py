7ea62d8ba47b7d69b200682c9bad2d1adff9ca25
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-12-24 19:33:20 +0800

    fix model loading

--- examples/reward_function/human_behaviour_tarpo.py
+++ examples/reward_function/human_behaviour_tarpo.py
@@ -162,7 +162,7 @@ def human_behaviour_compute_score_batch(
     # # default all to CLS semantics if not provided
     #     task_id = ["cls"] * len(solution_str)
     #NOTE: For debugging
-    task_id = ["qa"] * len(solution_str)
+    task_id = ["cls"] * len(solution_str)
 
     # print("Computing human behaviour reward scores...")
     # print(solution_str)
