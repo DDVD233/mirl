1bd5f7e9aa3618f4134af20d2466d5b64efa9df5
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2025-12-24 22:24:47 +0800

    fix val

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -974,8 +974,11 @@ class RayPPOTrainer:
  
             if "answer" in test_batch.non_tensor_batch:
                 non_tensor_batch_keys_to_pop.append("answer")
+            
             if "multi_modal_data" in test_batch.non_tensor_batch:
                 non_tensor_batch_keys_to_pop.append("multi_modal_data")
+            if "multi_modal_inputs" in test_batch.non_tensor_batch:
+                non_tensor_batch_keys_to_pop.append("multi_modal_inputs")
             if "raw_prompt" in test_batch.non_tensor_batch:
                 non_tensor_batch_keys_to_pop.append("raw_prompt")
             if "tools_kwargs" in test_batch.non_tensor_batch:
