181b6c2622fdb097914d65c496c019ad5c78a19e
keanepotato <keane.ongweiyang@u.nus.edu>
2025-12-26 03:07:40 -0500

    mib shell script

--- examples/tarpo_trainer/_mib_tarpo_full_trial_4gpus.sh
+++ examples/tarpo_trainer/_mib_tarpo_full_trial_4gpus.sh
@@ -2,7 +2,7 @@
 set -x
 
 # Pin to GPUs 0,1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 unset ROCR_VISIBLE_DEVICES
 export PYTHONUNBUFFERED=1
 export HYDRA_FULL_ERROR=1
@@ -30,8 +30,8 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 
     python3 -m verl.trainer.main_ppo \
         algorithm.adv_estimator=tarpo \
-        data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_train_cleaned_2.jsonl \
-        data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_val_cleaned.jsonl \
+        data.train_files=/scratch/keane/human_behaviour_data/final_v8_train_cleaned_2.jsonl \
+        data.val_files=/scratch/keane/human_behaviour_data/final_v8_val_cleaned.jsonl \
         data.train_batch_size=256 \
         data.val_batch_size=64 \
         data.max_prompt_length=4096 \
@@ -41,7 +41,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         data.image_key=images \
         data.video_key=videos \
         data.prompt_key=problem \
-        data.dataloader_num_workers=8 \
+        data.dataloader_num_workers=4 \
         data.modalities=\'audio,videos\' \
         data.train_modality_batching.enabled=True \
         data.train_modality_batching.drop_last=True \
@@ -52,7 +52,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         actor_rollout_ref.actor.optim.lr=5e-7 \
         actor_rollout_ref.model.use_remove_padding=True \
         actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-        actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+        actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
         actor_rollout_ref.actor.use_kl_loss=False \
         actor_rollout_ref.actor.kl_loss_coef=0 \
         actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -61,7 +61,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         actor_rollout_ref.model.enable_gradient_checkpointing=True \
         actor_rollout_ref.actor.fsdp_config.param_offload=False \
         actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-        actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
+        actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
         actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
         actor_rollout_ref.rollout.name=vllm \
         actor_rollout_ref.rollout.engine_kwargs.vllm.disable_mm_preprocessor_cache=False \
@@ -80,16 +80,16 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         reward_model.reward_manager=batch \
         trainer.critic_warmup=0 \
         trainer.logger='["console","wandb"]' \
-        trainer.project_name='rl_omni_heldout' \
-        trainer.experiment_name='tarpo_iter_16_new_std_mixtures_densities_ema_original' \
+        trainer.project_name='debug_rl' \
+        trainer.experiment_name='mib_tarpo_iter_16_mixtures_densities_ema_original' \
         trainer.n_gpus_per_node=4 \
         trainer.nnodes=1 \
         trainer.save_freq=50 \
         trainer.val_before_train=False \
         trainer.val_only=False \
-        trainer.validation_data_dir=/scratch/keane/human_behaviour/tarpo_iter_16_new_std_mixtures_densities_ema_original \
+        trainer.validation_data_dir=/scratch/keane/hb_atlas_models/tarpo_iter_16_mixtures_densities_ema_original \
         trainer.test_freq=50 \
         trainer.total_epochs=5 \
-        trainer.advantage_save_dir=/scratch/keane/human_behaviour/tarpo_iter_16_new_std_mixtures_densities_ema_original/advantages \
+        trainer.advantage_save_dir=/scratch/keane/hb_atlas_models/tarpo_iter_16_mixtures_densities_ema_original/advantages \
         trainer.advantage_plot_freq=15 $@ \
-        trainer.default_local_dir=/scratch/keane/human_behaviour/tarpo_iter_16_new_std_mixtures_densities_ema_original
+        trainer.default_local_dir=/scratch/keane/hb_atlas_models/tarpo_iter_16_mixtures_densities_ema_original
