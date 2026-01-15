bd330f21b98b991f90ad0b1b52cf67c87ca0603b
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-01-15 21:42:14 +0800

    _

--- examples/reward_function/llm_grader/run_llm_judge_eval.sh
+++ examples/reward_function/llm_grader/run_llm_judge_eval.sh
@@ -14,7 +14,7 @@ RESULTS_PATH="/scratch/keane/human_behaviour/naive_density/global_step_400_test/
 SAVE_PATH="/scratch/keane/human_behaviour/naive_density/global_step_400_test/full_test_or_val_generation_outputs/step_400_llm_grading_results.json"
 PROVIDER="openai"
 WANDB_PROJECT="llm_judge_eval"
-WANDB_RUN_NAME="task_only_harpo_full_experiment_$(date +%Y%m%d_%H%M%S)"
+WANDB_RUN_NAME="naive_density_harpo_full_experiment_$(date +%Y%m%d_%H%M%S)"
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
