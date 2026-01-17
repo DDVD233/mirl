5e05a2910c82749b5bf1e95fc6a33dc7c3db272d
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-01-17 18:08:46 +0800

    _

--- verl/trainer/ppo/utils/upload_advantage_distributions.py
+++ verl/trainer/ppo/utils/upload_advantage_distributions.py
@@ -8,7 +8,7 @@ ARTIFACT_NAME = "harpo_task_only_advantage_distribution"
 ARTIFACT_TYPE = "dataset"
 
 FOLDER_TO_ZIP = Path("/scratch/keane/human_behaviour/tarpo_iter_22_no_resp_no_hierarchy_mixtures_densities_ema_original/advantages")  # <-- set this
-ZIP_PATH = Path("/scratch/keane/human_behaviour/tarpo_iter_22_no_resp_no_hierarchy_mixtures_densities_ema_original/hier_advantages.zip")        # <-- output zip
+ZIP_PATH = Path("/scratch/keane/human_behaviour/tarpo_iter_22_no_resp_no_hierarchy_mixtures_densities_ema_original/harpo_task_only_advantages.zip")        # <-- output zip
 
 # ---- zip folder ----
 if not FOLDER_TO_ZIP.exists() or not FOLDER_TO_ZIP.is_dir():
