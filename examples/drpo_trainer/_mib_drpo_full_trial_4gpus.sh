0866a3c78e944b04cbf29e3190e1af09d40992cb
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-03-26 13:38:06 +0900

    _

--- examples/drpo_trainer/_mib_drpo_full_trial_4gpus.sh
+++ examples/drpo_trainer/_mib_drpo_full_trial_4gpus.sh
@@ -36,7 +36,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
     python3 -m verl.trainer.main_ppo \
         algorithm.adv_estimator=drpo \
         data.train_files=/scratch/keane/human_behaviour_data/final_v8_train_cleaned_2.jsonl \
-        data.val_files=/scratch/keane/human_behaviour_data/final_v8_val_cleaned.jsonl \
+        data.val_files=/scratch/keane/human_behaviour_data/final_v8_test_cleaned.jsonl \
         data.train_batch_size=256 \
         data.val_batch_size=8 \
         data.max_prompt_length=4096 \
