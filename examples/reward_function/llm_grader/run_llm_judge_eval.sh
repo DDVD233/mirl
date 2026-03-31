9a72ff411ead22c84c551ec76294634c308b74bf
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-03-31 17:44:33 +0900

    _

--- examples/reward_function/llm_grader/run_llm_judge_eval.sh
+++ examples/reward_function/llm_grader/run_llm_judge_eval.sh
@@ -9,11 +9,11 @@
 # RESULTS_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/input_data_2025-11-28_11-53-44.json"
 # SAVE_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/results_temp.json"
 
-RESULTS_PATH="/scratch/keane/human_behaviour/dapo_enhanced_harpo/global_step_350_test/full_test_or_val_generation_outputs/step_350.json"
-SAVE_PATH="/scratch/keane/human_behaviour/dapo_enhanced_harpo/global_step_350_test/full_test_or_val_generation_outputs/step_350_llm_grading_results.json"
+RESULTS_PATH="/scratch/keane/human_behaviour/emagrpo_engaging_baseline/full_test_or_val_generation_outputs/step_250.json"
+SAVE_PATH="/scratch/keane/human_behaviour/emagrpo_engaging_baseline/full_test_or_val_generation_outputs/step_350_llm_grading_results.json"
 PROVIDER="openai"
 WANDB_PROJECT="llm_judge_eval"
-WANDB_RUN_NAME="harpo_v2_experiment_$(date +%Y%m%d_%H%M%S)"
+WANDB_RUN_NAME="emagrpo_v1_experiment_$(date +%Y%m%d_%H%M%S)"
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
