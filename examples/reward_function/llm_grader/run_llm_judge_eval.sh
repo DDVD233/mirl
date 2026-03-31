48b7a3f96a39677185d816ecc15953c17ea64ebc
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-03-31 17:45:30 +0900

    _

--- examples/reward_function/llm_grader/run_llm_judge_eval.sh
+++ examples/reward_function/llm_grader/run_llm_judge_eval.sh
@@ -10,7 +10,7 @@
 # SAVE_PATH="/Users/keane/Desktop/research/human-behavior/verl/examples/reward_function/temp/results_temp.json"
 
 RESULTS_PATH="/scratch/keane/human_behaviour/emagrpo_engaging_baseline/full_test_or_val_generation_outputs/step_250.json"
-SAVE_PATH="/scratch/keane/human_behaviour/emagrpo_engaging_baseline/full_test_or_val_generation_outputs/step_350_llm_grading_results.json"
+SAVE_PATH="/scratch/keane/human_behaviour/emagrpo_engaging_baseline/full_test_or_val_generation_outputs/step_250_llm_grading_results.json"
 PROVIDER="openai"
 WANDB_PROJECT="llm_judge_eval"
 WANDB_RUN_NAME="emagrpo_v1_experiment_$(date +%Y%m%d_%H%M%S)"
