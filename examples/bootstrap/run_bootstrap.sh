b7c0aa62f0e6105ebedaf5d9fd2f236051159194
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-04-07 17:28:14 +0800

    _

--- examples/bootstrap/run_bootstrap.sh
+++ examples/bootstrap/run_bootstrap.sh
@@ -3,7 +3,7 @@
 LABEL_MAP="/home/keaneong/human-behavior/verl/sft/label_maps/unified_label_map.json"
 OUTPUT_JSON="/home/keaneong/human-behavior/verl/examples/bootstrap/v3_multi_model_eval_metrics_bootstrap.json"
 OUTPUT_MD="/home/keaneong/human-behavior/verl/examples/bootstrap/v3_multi_model_eval_metrics_bootstrap.md"
-N_BOOT=100000
+N_BOOT=1000
 
 # Which model name (must match a "name" entry below) is YOUR method for McNemar
 METHOD_NAME="harpo_method"
