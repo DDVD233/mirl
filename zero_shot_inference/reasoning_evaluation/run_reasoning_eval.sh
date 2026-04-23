1f1f557c13e9b5a1f21b577cf0fb4da9ed0f6d38
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-04-23 08:55:38 -0300

    _

--- zero_shot_inference/reasoning_evaluation/run_reasoning_eval.sh
+++ zero_shot_inference/reasoning_evaluation/run_reasoning_eval.sh
@@ -25,7 +25,7 @@ MODELS=(
 
 # GPUs to use. Leave empty to auto-detect all available GPUs.
 # Example: GPUS=(0 1)  or  GPUS=(2 3 4 5)
-GPUS=(0 1)
+GPUS=(0 1 7)
 
 # Number of concurrent (model, dataset) jobs per GPU.
 # Total parallel slots = NUM_GPUS × JOBS_PER_GPU.
