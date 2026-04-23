fdbcbb1f97ae9809484abf1ce9dac99e43b7daee
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-04-23 10:56:03 -0300

    _

--- zero_shot_inference/run_inference.sh
+++ zero_shot_inference/run_inference.sh
@@ -39,6 +39,7 @@ MODELS=(
 TEMPERATURE=1.0
 TOP_P=0.95
 TOP_K=64
+MIN_P=0
 # Directory where prediction JSONLs and metrics are written
 
 OUTPUT_DIR="/home/keaneong/human-behavior/verl/zero_shot_inference/results/thinking_w_params"
