6954fe72a3a18d9037e1378f4acd23812d1986e5
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-04-23 07:55:05 -0300

    fix paths

--- zero_shot_inference/reasoning_evaluation/run_reasoning_eval.sh
+++ zero_shot_inference/reasoning_evaluation/run_reasoning_eval.sh
@@ -71,8 +71,8 @@ DATASETS=(
 
 EATD_JSONL="/scratch/keane/hb_generalization_data/MVSA_EATD_zeroshot/test_eatd_prompts.jsonl"
 MVSA_JSONL="/scratch/keane/hb_generalization_data/MVSA_EATD_zeroshot/test_mvsa_prompts.jsonl"
-AVASD_JSONL="/scratch/keane/hb_generalization_data/zero_shot_data/test_av_asd_promptsmultilabel.jsonl"
-IEMOCAP_JSONL="/scratch/keane/hb_generalization_data/zero_shot_data/latest_iemocap_test.jsonl"
+AVASD_JSONL="/scratch/keane/hb_generalization_data/avasd/test_av_asd_promptsmultilabel.jsonl"
+IEMOCAP_JSONL="/scratch/keane/hb_generalization_data/iemocap/latest_iemocap_test.jsonl"
 DREADDIT_JSONL="/scratch/keane/hb_generalization_data/zero_shot_data_v2/test_dreaddit_prompts.jsonl"
 SARCNET_JSONL="/scratch/keane/hb_generalization_data/zero_shot_data_v2/test_sarcnet_prompts.jsonl"
 
