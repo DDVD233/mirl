ef4cf6d4575c7fb636b649fa2b6a2a1e56e87551
Keane Ong Wei Yang <keane.ongweiyang@u.nus.edu>
2026-04-24 12:16:21 -0300

    _

--- examples/gpg_trainer/_engaging_gpg_full_2gpus_resume.sh
+++ examples/gpg_trainer/_engaging_gpg_full_2gpus_resume.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
+# Pin to GPUs 0,1
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
 export CUDA_VISIBLE_DEVICES=0,1
 unset ROCR_VISIBLE_DEVICES
 export PYTHONUNBUFFERED=1
@@ -8,12 +10,36 @@ export HYDRA_FULL_ERROR=1
 export PYTHONPATH="/home/keaneong/human-behavior/verl:$PYTHONPATH"
 export NCCL_ASYNC_ERROR_HANDLING=1
 
+
+# train modality batching = do one modality at a time;
+
+# --train_file "/scratch/keane/human_behaviour/human_behaviour_data/w_feats_v6_exclude_heldout_train.jsonl" \
+# --val_file "/scratch/keane/human_behaviour/human_behaviour_data/w_feats_v6_exclude_heldout_val.jsonl" \
+
+# the printed out total steps will essentially be the all the steps within the full number of epochs (i.e. 1/1210, where epoch is 5), 1210 is num of steps for 5 epochs
+# take 1210/5 = 242 as steps per epoch
+# hence we should eval every 242 but save every 121
+
+# list of edits done:
+# free_cache_engine:changed from true to false for speed at the expense of memory (failed)
+# actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \ set to 2 from 1 
+# actor_rollout_ref.model.use_remove_padding=True \ set to true from false 
+# max prompt length and max response length set to 2048 from 4096
+# dataloader num workers set to 8
+# gpu memory set to 0.7 from 0.6
+
+# originally prompt length, response length, max model len is 2048, 2048, 8192 
+
+# data.train_files=/scratch/keane/human_behaviour_data/final_v8_train_cleaned_2.jsonl \
+# To avoid multimodal request cache error, do max_num_batched_tokens = 3072
+# And also the micro batch size per gpu to 8
+
     python3 -m verl.trainer.main_ppo \
-        algorithm.adv_estimator=emagrpo \
+        algorithm.adv_estimator=gpg \
         data.train_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_train_cleaned_2.jsonl \
-        data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_test_cleaned.jsonl \
+        data.val_files=/scratch/keane/human_behaviour/human_behaviour_data/final_v8_val_cleaned.jsonl \
         data.train_batch_size=256 \
-        data.val_batch_size=64 \
+        data.val_batch_size=8 \
         data.max_prompt_length=4096 \
         data.max_response_length=2048 \
         data.filter_overlong_prompts=False \
@@ -29,19 +55,19 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         data.val_modality_batching.drop_last=False \
         data.format_prompt=/home/keaneong/human-behavior/verl/examples/format_prompt/default.jinja \
         actor_rollout_ref.model.path=Qwen/Qwen2.5-Omni-7B \
-        actor_rollout_ref.actor.optim.lr=2e-6 \
+        actor_rollout_ref.actor.optim.lr=1e-6 \
         actor_rollout_ref.model.use_remove_padding=True \
         actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-        actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
-        actor_rollout_ref.actor.use_kl_loss=True \
+        actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
+        actor_rollout_ref.actor.use_kl_loss=False \
         actor_rollout_ref.actor.kl_loss_coef=0 \
         actor_rollout_ref.actor.kl_loss_type=low_var_kl \
         actor_rollout_ref.actor.entropy_coeff=0 \
         actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
         actor_rollout_ref.model.enable_gradient_checkpointing=True \
-        actor_rollout_ref.actor.fsdp_config.param_offload=False \
-        actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-        actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+        actor_rollout_ref.actor.fsdp_config.param_offload=True \
+        actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+        actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
         actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
         actor_rollout_ref.rollout.name=vllm \
         actor_rollout_ref.rollout.engine_kwargs.vllm.disable_mm_preprocessor_cache=True \
@@ -50,10 +76,10 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         actor_rollout_ref.rollout.enforce_eager=False \
         actor_rollout_ref.rollout.free_cache_engine=True \
         actor_rollout_ref.rollout.n=3 \
-        actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
+        actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
         actor_rollout_ref.ref.fsdp_config.param_offload=True \
         actor_rollout_ref.rollout.max_model_len=6192 \
-        actor_rollout_ref.rollout.max_num_batched_tokens=6192 \
+        actor_rollout_ref.rollout.max_num_batched_tokens=3096 \
         algorithm.use_kl_in_reward=False \
         custom_reward_function.path=/home/keaneong/human-behavior/verl/examples/reward_function/human_behaviour_tarpo.py \
         custom_reward_function.name=human_behaviour_compute_score_batch \
@@ -61,15 +87,18 @@ export NCCL_ASYNC_ERROR_HANDLING=1
         trainer.critic_warmup=0 \
         trainer.logger='["console","wandb"]' \
         trainer.project_name='rl_omni_heldout' \
-        trainer.experiment_name='emagrpo_engaging_baseline' \
+        trainer.experiment_name='22apr_gpg' \
         trainer.n_gpus_per_node=2 \
         trainer.nnodes=1 \
         trainer.save_freq=50 \
         trainer.val_before_train=False \
         trainer.val_only=False \
-        trainer.validation_data_dir=/scratch/keane/human_behaviour/emagrpo_engaging_baseline \
+        trainer.validation_data_dir=/scratch/keane/human_behaviour/22apr_gpg \
         trainer.test_freq=99999 \
         trainer.total_epochs=5 \
-        trainer.advantage_save_dir=/scratch/keane/human_behaviour/emagrpo_engaging_baseline/advantages \
+        trainer.advantage_save_dir=/scratch/keane/human_behaviour/22apr_gpg/advantages \
         trainer.advantage_plot_freq=15 $@ \
-        trainer.default_local_dir=/scratch/keane/human_behaviour/emagrpo_engaging_baseline
+        trainer.default_local_dir=/scratch/keane/human_behaviour/22apr_gpg
+
+        # trainer.resume_mode=resume_path \
+        # trainer.resume_from_path=/scratch/keane/hb_atlas_models/harpo_no_ema/global_step_350 \
