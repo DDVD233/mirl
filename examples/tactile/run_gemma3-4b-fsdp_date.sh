eb8d33e1814f47da6157cc32eb6a725131e6bf6e
DVD <zjdavid.2003@gmail.com>
2026-05-07 17:56:48 -0400

    Set Gemma3DecoderLayer for FSDP wrap policy in gemma-3 script

--- examples/tactile/run_gemma3-4b-fsdp_date.sh
+++ examples/tactile/run_gemma3-4b-fsdp_date.sh
@@ -39,6 +39,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.actor.fsdp_config.ulysses_sequence_parallel_size=$SP_SIZE \
+    +actor_rollout_ref.actor.fsdp_config.wrap_policy.transformer_layer_cls_to_wrap=[Gemma3DecoderLayer] \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
@@ -52,6 +53,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.fsdp_config.ulysses_sequence_parallel_size=$SP_SIZE \
+    +actor_rollout_ref.ref.fsdp_config.wrap_policy.transformer_layer_cls_to_wrap=[Gemma3DecoderLayer] \
     reward_model.reward_manager=dapo \
     +reward_model.reward_kwargs.overlong_buffer_cfg.enable=True \
     +reward_model.reward_kwargs.overlong_buffer_cfg.len=512 \
