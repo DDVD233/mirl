81129bc84a0c283053b8f19149ad94fb6629af2e
DVD <zjdavid.2003@gmail.com>
2026-05-09 10:10:30 -0400

    Bump gemma3 weight-update bucket to 4096MB for fp32 embed_tokens

--- examples/tactile/run_gemma3-4b-fsdp_date.sh
+++ examples/tactile/run_gemma3-4b-fsdp_date.sh
@@ -48,6 +48,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.update_weights_bucket_megabytes=4096 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.load_format=auto \
