bca77622a83f18dbd0a0e74ae393ed34926719c7
DVD <zjdavid.2003@gmail.com>
2026-05-08 20:38:49 -0400

    Fix path: bucket_megabytes is under rollout.checkpoint_engine

--- examples/tactile/run_qwen3_vl-8b-fsdp_date.sh
+++ examples/tactile/run_qwen3_vl-8b-fsdp_date.sh
@@ -44,7 +44,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
-    actor_rollout_ref.rollout.update_weights_bucket_megabytes=4096 \
+    actor_rollout_ref.rollout.checkpoint_engine.update_weights_bucket_megabytes=4096 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=12288 \
