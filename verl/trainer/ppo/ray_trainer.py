3ff4ad87afbba8d1dd07df97abad7af2c0cd138f
DVD <zjdavid.2003@gmail.com>
2026-05-08 23:58:06 -0400

    Drop VERL_SKIP_INIT_SYNC bypass (broke wake_up cycle, CUDA illegal addr)

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1268,10 +1268,7 @@ class RayPPOTrainer:
 
         # load checkpoint and update weights before doing anything
         self._load_checkpoint()
-        if os.environ.get("VERL_SKIP_INIT_SYNC", "0") != "1":
-            self.checkpoint_manager.update_weights(self.global_steps)
-        else:
-            print("[verl-debug] VERL_SKIP_INIT_SYNC=1 -> skipping initial FSDP->rollout sync", flush=True)
+        self.checkpoint_manager.update_weights(self.global_steps)
 
         current_epoch = self.global_steps // len(self.train_dataloader)
 
